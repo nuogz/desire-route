75d8ef12ffda015b02f4c1a077c42f4c3d017157
Nuogz <zheung@vip.qq.com>
2022-06-01 17:04:46 +0800

    v1.0.0 initial version

---
+++ README.md
@@ -0,0 +1,5 @@
+# desire-route
+![Version](https://img.shields.io/github/package-json/v/zheung/desire-route?style=flat-square)
+[![License](https://img.shields.io/github/license/zheung/desire-route?style=flat-square)](https://www.gnu.org/licenses/lgpl-3.0-standalone.html)
+
+A simple route loader for [the default harb of desire](https://github.com/zheung/desire-harb-default).
---
+++ index.js
@@ -0,0 +1,51 @@
+import { resolve, parse, sep, basename, posix } from 'path';
+import { pathToFileURL } from 'url';
+
+import ReaddirRecur from 'fs-readdir-recursive';
+
+
+
+export default async function readRoute(pathSource) {
+	const faces = [];
+	const folds = [];
+
+	const filesSource = ReaddirRecur(pathSource);
+
+
+	const filesAPI = filesSource.filter(f => f.endsWith('.api.js'));
+	for(const fileAPI of filesAPI) {
+		const pathAPI = resolve(pathSource, fileAPI);
+		const infoAPI = parse(fileAPI);
+
+		const api = await import(pathToFileURL(pathAPI));
+
+		faces.push({
+			route: posix.join(
+				...infoAPI.dir.split(sep),
+				basename(infoAPI.base, '.api.js')
+			),
+			method: api.method ?? 'get',
+			handle: api.handle,
+			upload: api.upload ?? false,
+			option: Object.assign({ parseResult: true }, JSON.parse(JSON.stringify(api))),
+		});
+	}
+
+
+	const filesMAP = filesSource.filter(p => p.endsWith('.map.js'));
+	for(const fileMAP of filesMAP) {
+		const pathMAP = resolve(pathSource, fileMAP);
+		const dirMAP = parse(pathMAP).dir;
+
+		const maps = (await import(pathToFileURL(pathMAP))).default;
+
+		maps.forEach(({ route, path }) =>
+			folds.push({
+				route,
+				path: resolve(dirMAP, path)
+			})
+		);
+	}
+
+	return { faces, folds };
+}
